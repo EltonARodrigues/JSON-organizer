e1a0cfa48df9b825ce2af97306e590293704641b
Elton de Andrade Rodrigues <eltonrodrigues61@gmail.com>
2019-05-20 15:54:46 -0300

    More test and refactored code

--- json_organizer/message.py
+++ json_organizer/message.py
@@ -1,8 +1,8 @@
 from ast import literal_eval
 from datetime import datetime, timedelta
 from .exception import InvalidMessage
-import os
 import datetime
+import os
 import json
 import re
 import csv
@@ -15,6 +15,24 @@ class Message():
 
     def __str__(self):
         return F"INPUT: {self.__message}"
+
+    def _verify_fields(self):
+        return "id" in self.__message and re.findall(
+            self.__keys, str(self.__message))
+
+    def _get_measurer_key(self):
+        return [key for key in self.__message.keys()
+                if re.search(self.__keys, key)][0]
+
+    def _define_date(self, date):
+        return date - timedelta(minutes=-1)
+
+    def _format_date(self, date):
+        return date.strftime("%Y-%m-%d@%H:%M:%S")
+
+    def _count_values(self):
+        measurer_key = self._get_measurer_key()
+        return len(self.__message[measurer_key]) - 1
 
     def validate(self):
         try:
@@ -29,54 +47,39 @@ class Message():
             raise InvalidMessage("Error with dict")
             return False
 
-    def _verify_fields(self):
-        return "id" in self.__message and re.findall(self.__keys, str(self.__message))
-
-
-    def formt_to_api(self):
-        api_template = {}
-        measurer_key = [key for key in self.__message.keys()
-                        if re.search(self.__keys, key)][0]
-        count_v = len(self.__message[measurer_key])
+    def format_to_api(self):
         api_join_menssage = []
+
+        measurer_key = self._get_measurer_key()
+        count = self._count_values()
+
         date = datetime.datetime.now()
-        for i in range(count_v):
-            date_format = date - timedelta(minutes=(count_v - (i + 1)))
-            api_template["values"] = self.__message[measurer_key][i]
-            api_template["date"] = date_format.strftime("%Y-%m-%d@%H:%M:%S")
-            api_join_menssage.append(dict(api_template))
+
+        for i in range(count + 1):
+            date = self._define_date(date)
+            api_template = {
+                'values': self.__message[measurer_key][i],
+                'date': self._format_date(date)
+            }
+            api_join_menssage.append(api_template)
         return api_join_menssage
 
     def last_value(self):
-        message_to_send = {}
-        formatada = self.__message
-        measurer_key = [key for key in self.__message.keys()
-                        if re.search(self.__keys, key)][0]
+        measurer_key = self._get_measurer_key()
 
-        return "{\"" + str(measurer_key) + "\":\"" + str(
-            self.__message[measurer_key][(len(self.__message[measurer_key]) - 1)]) + "\"}"
+        last = {
+            measurer_key: self.__message[measurer_key][self._count_values()]
+        }
+        return last
 
-    def format_dojot_message(self):
-        measurer_key = [key for key in self.__message.keys()
-                        if re.search(self.__keys, key)][0]
-        count_v = len(list(filter(None, self.__message[measurer_key])))
-        api_template = "{" + measurer_key + ": "" }"
-        date = datetime.datetime.now()
-        for i in range(int(10)):
-            date_format = date - timedelta(minutes=(count_v - (i + 1)))
-            api_template['values'] = self.__message[measurer_key][i]
-            api_template['date'] = date_format.strftime('%Y-%m-%d@%H:%M:%S')
-        print(api_template)
-        return api_template
-
-    # AWS
     def format_aws_message(self):
-        measurer_key = [key for key in self.__message.keys()
-                        if re.search(self.__keys, key)][0]
-        json_m = self.__message
+        measurer_key = self._get_measurer_key()
+
         now = datetime.datetime.now()
         data = now.strftime("%Y-%m-%d %H:%M")
-        fmessage = {"id": (json_m["id"]), "vol": (
-            json_m[measurer_key][(len(self.__message[measurer_key]) - 1)]), "data": data}
-
-        return (fmessage)
+        aws_message = {
+            "id": self.__message['id'],
+            "vol": self.__message[measurer_key][self._count_values()],
+            "data": data
+        }
+        return aws_message
--- tests/test_message.py
+++ tests/test_message.py
@@ -1,20 +1,63 @@
+from datetime import datetime, timedelta
 from json_organizer.message import Message
 from json_organizer.exception import InvalidMessage
 
 from unittest import TestCase
+import datetime
 
 
 class Testmessage(TestCase):
 
     def setUp(self):
-        self.input = '{"id":"fce176","topic":"bancada","countpkt":"2","sensor3":[159.99,174.47,189.03,203.51,218.07,232.55,247.03,261.50,275.98,290.55]}'
+        input = '''{"id":"fce176","topic":"bancada",
+                    "countpkt":"2","sensor3":[159.99,174.47,
+                    189.03,203.51,218.07,232.55,247.03,261.50,
+                    275.98,290.55]}'''
+
+        self.message = Message(input)
+        self.validate = self.message.validate()
 
     def test_message_validation_is_true(self):
-        message = Message(self.input)
-        validator = message.validate()
-        self.assertEqual(True, validator)
+        self.assertEqual(True, self.validate)
 
     def test_message_validation_is_false(self):
         with self.assertRaises(InvalidMessage):
-            message = Message('{"id":"fce176')
-            validator = message.validate()
+            self.message = Message(input)
+            self.validate = self.message.validate()
+
+    def test_message_validation_is_true(self):
+        self.assertEqual(True, self.validate)
+
+    def test_message_validation_is_false(self):
+        with self.assertRaises(InvalidMessage):
+            wrong_message = Message('{"id":"fce176')
+            validade = wrong_message.validate()
+
+    def test_format_to_api(self):
+        output = self.message.format_to_api()
+
+        self.assertEqual(type(output), list)
+
+    def test_format_to_api(self):
+        output = self.message.format_to_api()
+
+        self.assertEqual(type(output), list)
+
+    def test_last_value(self):
+        output = self.message.last_value()
+        value_test = {"sensor3": 290.55}
+
+        self.assertEqual(output, value_test)
+
+    def test_aws_message(self):
+        now = datetime.datetime.now()
+        date = now.strftime("%Y-%m-%d %H:%M")
+
+        value = {
+            'id': 'fce176',
+            'vol': 290.55,
+            'data': date
+        }
+
+        output = self.message.format_aws_message()
+        self.assertEqual(output, value)
